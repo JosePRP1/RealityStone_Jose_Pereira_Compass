b8232d2ad3f06563ce7409df949ecdae7ef063b9
Jose Paulo <zezincria123@gmail.com>
2023-08-11 20:22:55 -0300

    alterações do dia

---
+++ aulas one bit code/aula2.rb
@@ -0,0 +1,15 @@
+nome = gets.chomp
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome}, tem #{idade} anos"
+
+
+
+num1 = gets.chomp.to_f
+num2 = gets.chomp.to_f
+
+soma = num1 + num2
+subt = num1 - num2
+divs = num1 / num2
+
+puts "Soma = #{soma}, Subtração = #{subt}, Divisão = #{divs}"
